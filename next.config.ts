438d123c57b647fe008d25a04d6343a0d6a1612c
Arbiteros <arbiteros@MacBook-Pro.local>
2026-01-22 00:09:08 +0700

    add dockerfile for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    reactStrictMode: false,
-
+  reactStrictMode: false,
+  output: 'standalone',
   /* config options here */
 };
 
