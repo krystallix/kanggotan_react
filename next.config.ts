3d4c23d0055e9011c541d5cb47a5dda3c384b3b6
Arbiteros <arbiteros@MacBook-Pro.local>
2026-01-23 20:11:19 +0700

    cache-control

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,28 @@ const nextConfig: NextConfig = {
     unoptimized: true,
   },
   compress: true,
+  async headers() {
+    return [
+      {
+        source: '/:all*(svg|jpg|jpeg|png|gif|ico|webp)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+      {
+        source: '/_next/static/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
